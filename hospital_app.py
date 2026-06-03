c970878945030265184bd8485ababae990c067c2
annisahakimi <annisa.hakimi@kodingnext.com>
2026-06-03 10:52:33 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -1,1 +1,24 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pickle
+import os
 
+# title in html
+st.set_page_config(page_title = "Smart Hospital Patient Navigator", page_icon = "🏥",layout = "wide")
+
+st.markdown("""
+  <style>
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
+
+    html,body, [class"="css"] {font-family: 'Inter', sans-serif}
+
+    #Main Menu {visibility: hidden;}
+    header[data-testid="stHeader"]{display:none;}
+    
+    
+  </style>
+""")
+
+
+                   
